3318783daacfd62247fdcd294ce87cd8eaa6344d
tmrizzo <tiagomdrizzo@gmail.com>
2018-08-17 00:08:46 -0300

    Changes to use rofi and KDE. Also Fedora 28 support

--- screenlayout/displays_home2.sh
+++ screenlayout/displays_home2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --mode 1360x768 --pos 1600x0 --rotate normal --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP-1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2-1 --mode 1360x768 --pos 1600x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP-2 --off
